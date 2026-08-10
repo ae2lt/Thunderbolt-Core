5a98487da42904d56c5330678be55131c6071929
syarukasu <187626967+syarukasu@users.noreply.github.com>
2026-08-10 13:30:04 +0900

    Add Forge 1.20.1 mod constructor

--- src/main/java/com/moakiee/thunderbolt/ThunderboltCore.java
+++ src/main/java/com/moakiee/thunderbolt/ThunderboltCore.java
@@ -9,6 +9,7 @@ import com.mojang.logging.LogUtils;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.event.server.ServerStoppedEvent;
@@ -19,7 +20,12 @@ public final class ThunderboltCore {
    public static final String MODID = "thunderbolt";
    public static final Logger LOGGER = LogUtils.getLogger();
 
-   public ThunderboltCore(IEventBus modEventBus) {
+   public ThunderboltCore() {
+      // Forge 1.20.1は@Modクラスを引数なしで生成するため、現在のMODイベントバスを明示的に取得する。
+      this(FMLJavaModLoadingContext.get().getModEventBus());
+   }
+
+   private ThunderboltCore(IEventBus modEventBus) {
       ThunderboltBlockEntities.TYPES.register(modEventBus);
       modEventBus.addListener(this::onCommonSetup);
       MinecraftForge.EVENT_BUS.addListener(this::onServerStarting);
