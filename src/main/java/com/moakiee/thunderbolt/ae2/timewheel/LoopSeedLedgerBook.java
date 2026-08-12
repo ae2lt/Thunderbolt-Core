72a5d1279bd7e79b0e167b3c6fb5ba38dd945e19
MOAKIEE <1324227985@qq.com>
2026-08-12 16:22:42 +0800

    chore: remove unused imports

--- src/main/java/com/moakiee/thunderbolt/ae2/timewheel/LoopSeedLedgerBook.java
+++ src/main/java/com/moakiee/thunderbolt/ae2/timewheel/LoopSeedLedgerBook.java
@@ -15,7 +15,6 @@ import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 import java.util.function.Predicate;
--- src/test/java/com/moakiee/thunderbolt/ae2/crafting/FastCraftingPlannerIdOnlyCraftableVariantTest.java
+++ src/test/java/com/moakiee/thunderbolt/ae2/crafting/FastCraftingPlannerIdOnlyCraftableVariantTest.java
@@ -11,7 +11,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.UUID;
 import java.util.concurrent.Future;
 
 import com.google.common.collect.ImmutableSet;
