f2a8a2c823c5c41d8be9140cffecca89f8f1271e
MOAKIEE <1324227985@qq.com>
2026-08-25 11:28:56 +0800

    feat(gui): port algorithm provider screen restyle to Forge 1.20.1

--- src/main/java/com/moakiee/thunderbolt/core/crafting/algorithm/client/ThunderboltClientEvents.java
+++ src/main/java/com/moakiee/thunderbolt/core/crafting/algorithm/client/ThunderboltClientEvents.java
@@ -1,13 +1,18 @@
 package com.moakiee.thunderbolt.core.crafting.algorithm.client;
 
 import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
+import appeng.client.gui.style.StyleManager;
+
 import com.moakiee.thunderbolt.ThunderboltCore;
 import com.moakiee.thunderbolt.core.crafting.algorithm.ThunderboltMenus;
+import com.moakiee.thunderbolt.core.crafting.algorithm.menu.CraftingAlgorithmProviderMenu;
 
 @Mod.EventBusSubscriber(
         modid = ThunderboltCore.MODID,
@@ -21,6 +26,13 @@ public final class ThunderboltClientEvents {
     public static void registerScreens(FMLClientSetupEvent event) {
         event.enqueueWork(() -> MenuScreens.register(
                 ThunderboltMenus.CRAFTING_ALGORITHM_PROVIDER.get(),
-                CraftingAlgorithmProviderScreen::new));
+                ThunderboltClientEvents::createCraftingAlgorithmProviderScreen));
+    }
+
+    private static CraftingAlgorithmProviderScreen createCraftingAlgorithmProviderScreen(
+            CraftingAlgorithmProviderMenu menu, Inventory inventory, Component title) {
+        var style = StyleManager.loadStyleDoc(
+                "/screens/thunderbolt/crafting_algorithm_provider.json");
+        return new CraftingAlgorithmProviderScreen(menu, inventory, title, style);
     }
 }
